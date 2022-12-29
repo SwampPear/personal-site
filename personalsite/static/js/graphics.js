5dea79a1bd2553c050417113ca719d71d04d5522
Michael Vaden <michaeljosephvaden03@gmail.com>
2022-12-28 22:44:56 -0500

    added 'three' to '.gitignore'

--- personalsite/static/js/graphics.js
+++ personalsite/static/js/graphics.js
@@ -4,12 +4,13 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const graphics = document.querySelector('.graphics')
 
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth / 2, window.innerHeight / 2);
+const renderer = new THREE.WebGLRenderer({alpha: true, antialias: true});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setClearColor( 0x000000, 0 )
 graphics.appendChild(renderer.domElement);
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshBasicMaterial( { color: 0x6ab0ed } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -18,7 +19,7 @@ camera.position.z = 5;
 function animate() {
     requestAnimationFrame( animate );
 
-    cube.rotation.x += 0.01;
+    cube.rotation.x += 0.001;
     cube.rotation.y += 0.01;
 
     renderer.render( scene, camera );
