5366ab6e5bdf18db4e33b93dda888bd0421adbb3
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-01 20:54:54 -0500

    implemented navbar

---
+++ personalsite/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import { Nav } from './features/nav/Nav'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav/>
+    </div>
+  );
+}
+
+export default App;
---
+++ personalsite/src/features/nav/Nav.tsx
@@ -0,0 +1,13 @@
+import styles from './Nav.module.css'
+
+export function Nav() {
+  return (
+    <nav>
+        <div className={styles.content}>
+            <a href="https://www.google.com" className={styles.scrollButton}>Work</a>
+            <a href="https://www.google.com" className={styles.scrollButton}>Posts</a>
+            <a href="https://www.google.com" className={styles.scrollButton}>Contact</a>
+        </div>
+    </nav>
+  );
+}
