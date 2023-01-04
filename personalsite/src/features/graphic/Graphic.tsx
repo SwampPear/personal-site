8f6f2c700de8f611e5d7214d89da5d7796a7d851
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-04 18:31:08 -0500

    got threejs working and implemented boilerplate

--- personalsite/src/features/graphic/Graphic.tsx
+++ personalsite/src/features/graphic/Graphic.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import styles from './Graphic.module.css'
+import * as THREE from 'three'
 
 export function Graphic() {
+    
+  useEffect(() => {
+    // setup
+    const scene = new THREE.Scene()
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+    const graphics = document.querySelector('.container')
+  })
   return (
     <div className={styles.container}>
-
+        
     </div>
   );
 }
