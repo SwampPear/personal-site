d17c29e756df1d524e760576c6b8a29a6e74c9bd
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-04 18:09:23 -0500

    implemented icon for header

--- personalsite/src/features/header/Header.tsx
+++ personalsite/src/features/header/Header.tsx
@@ -1,14 +1,19 @@
 import styles from './Header.module.css'
+import logo from '../../../src/gear.svg'
+import darkmode from '../../../src/sun.svg'
+import { Icon } from '../icon/Icon'
 
 export function Header() {
   return (
     <header>
         <div className={styles.brand}>
+        <Icon />
             <span className={styles.title}>
                 Michael Vaden
             </span>
         </div>
         <button className={styles.darkmode}>
+            <img src={darkmode}/>
         </button>
     </header>
   );
