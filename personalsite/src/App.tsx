629cf0db8081f16497be80dc16dd05540bd90dc3
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-04 18:22:40 -0500

    implemented boilerplate for graphic

--- personalsite/src/App.tsx
+++ personalsite/src/App.tsx
@@ -3,12 +3,15 @@ import { Counter } from './features/counter/Counter';
 import { Nav } from './features/nav/Nav'
 import './App.css';
 import { Header } from './features/header/Header';
+import { Graphic } from './features/graphic/Graphic'
 
 function App() {
   return (
     <div className="App">
       <Nav/>
       <Header/>
+      <Graphic />
+
     </div>
   );
 }
---
+++ personalsite/src/features/graphic/Graphic.tsx
@@ -0,0 +1,9 @@
+import styles from './Graphic.module.css'
+
+export function Graphic() {
+  return (
+    <div className={styles.container}>
+
+    </div>
+  );
+}
