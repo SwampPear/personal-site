6d96b580cd0a313dfb6e63a7a5bbe45257fae43c
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-05 18:48:36 -0500

    implemented darkmode for nav

--- personalsite/src/features/nav/Nav.tsx
+++ personalsite/src/features/nav/Nav.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import styles from './Nav.module.css'
 
+import { useAppSelector } from '../../app/hooks'
+import { selectDarkModeState, selectDarkModeInit } from '../darkmode/darkModeSlice'
+
 const Nav = () => {
+  const darkmode = useAppSelector(selectDarkModeState)
+  const darkmodeInit = useAppSelector(selectDarkModeInit)
+
   return (
     <nav>
         <div className={styles.content}>
-            <a href="https://www.google.com" className={styles.scrollButton}>Work</a>
-            <a href="https://www.google.com" className={styles.scrollButton}>Posts</a>
-            <a href="https://www.google.com" className={styles.scrollButton}>Contact</a>
+            <a href="https://www.google.com" className={`${styles.scrollButton} ${darkmodeInit ? (darkmode ? styles.scrollButtonToDark : styles.scrollButtonFromDark) : ''}`}>Work</a>
+            <a href="https://www.google.com" className={`${styles.scrollButton} ${darkmodeInit ? (darkmode ? styles.scrollButtonToDark : styles.scrollButtonFromDark) : ''}`}>Posts</a>
+            <a href="https://www.google.com" className={`${styles.scrollButton} ${darkmodeInit ? (darkmode ? styles.scrollButtonToDark : styles.scrollButtonFromDark) : ''}`}>Contact</a>
         </div>
     </nav>
   );
