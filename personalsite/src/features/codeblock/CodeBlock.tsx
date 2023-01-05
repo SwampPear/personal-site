f46ba2bc2497ec2a30350849fe7b96d6b5c95d5f
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-05 15:46:31 -0500

    started implementation of color-based linter for codeblock

--- personalsite/src/features/codeblock/CodeBlock.tsx
+++ personalsite/src/features/codeblock/CodeBlock.tsx
@@ -3,8 +3,20 @@ import styles from './CodeBlock.module.css'
 
 
 const CodeBlock = () => {
+  const colors = {
+    'pink':	'#ff7edB',
+    'yellow': 	'#fede5d',
+    'orange': 	'#ff8b39',
+    'gray': '#bbbbbb'
+  }
+
   return (
     <div className={styles.container}>
+      <span style={{ color: '#fede5d'}}className={styles.code}>import </span>
+      <span style={{ color: '#ff7edB'}}className={styles.code}>React </span>
+      <span style={{ color: '#fede5d'}}className={styles.code}>from </span>
+      <span style={{ color: '#ff8b39'}}className={styles.code}>'react'</span>
+      <span style={{ color: '#bbbbbb'}}className={styles.code}>;</span>
     </div>
   );
 }
