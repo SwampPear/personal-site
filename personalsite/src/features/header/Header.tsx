45868e1465d846529239cdd66a11c31f92263cb8
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-05 18:34:33 -0500

    implemented darkmode with header

--- personalsite/src/features/header/Header.tsx
+++ personalsite/src/features/header/Header.tsx
@@ -3,9 +3,16 @@ import styles from './Header.module.css'
 import Icon from '../icon/Icon'
 import DarkMode from '../darkmode/DarkMode' 
 
+import { useAppSelector  } from '../../app/hooks'; 
+import { selectDarkModeState, selectDarkModeInit } from '../darkmode/darkModeSlice';
+
+
 const Header = () => {
+  const darkmode = useAppSelector(selectDarkModeState)
+  const darkmodeInit = useAppSelector(selectDarkModeInit)
+
   return (
-    <header>
+    <header className={`${darkmodeInit ? (darkmode ? styles.headerToDark : styles.headerFromDark) : ''}`}>
         <div className={styles.brand}>
         <Icon />
             <span className={styles.title}>
