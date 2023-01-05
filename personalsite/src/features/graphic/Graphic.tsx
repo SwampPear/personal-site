5573cba9d1acc24087e233ec4c51a49747cbc660
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-04 21:05:12 -0500

    got threejs working but need to fix bug where three canvas renders twice

--- personalsite/src/features/graphic/Graphic.tsx
+++ personalsite/src/features/graphic/Graphic.tsx
@@ -2,36 +2,42 @@
 import styles from './Graphic.module.css'
 import * as THREE from 'three'
 
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 
 export function Graphic() {
-  /*
+  const mountRef = useRef<HTMLDivElement>(null)
+
   useEffect(() => {
-    // setup
-    //const scene = new THREE.Scene()
-    //const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
-    //const graphics = document.querySelector('.container')
+
     var scene = new THREE.Scene();
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
+    
     renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    
+    mountRef.current!.appendChild( renderer.domElement );
+    
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
+    
     scene.add( cube );
     camera.position.z = 5;
+    
     var animate = function () {
       requestAnimationFrame( animate );
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
       renderer.render( scene, camera );
-    };
-    animate();
-  }, [])
-  */
+    }
+    
+    animate()
+
+    //mountRef.current!.removeChild( renderer.domElement )
+  }, []);
+  
   return (
-    <div>
+    <div className={styles.container} ref={mountRef}>
     </div>
   );
 }
