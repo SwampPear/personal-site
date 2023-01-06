4ad780a8047095d141a8bc78101ee9de0cb4f50b
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-06 09:28:44 -0500

    implemented 'Post' model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    postId = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=127)
+    reading_time = models.IntegerField(default=5)
+    content = models.JSONField()
