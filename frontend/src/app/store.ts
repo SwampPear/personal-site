759ea26aab6589ce2e701c88c33c77b13b89089a
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-05 22:19:26 -0500

    implemented slice for nav

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,13 +1,15 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import darkModeReducer from '../features/darkmode/darkModeSlice'
+import navReducer from '../features/nav/navSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
-    darkMode: darkModeReducer
-  },
-});
+    darkMode: darkModeReducer,
+    nav: navReducer
+  }
+})
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
---
+++ frontend/src/features/nav/navSlice.ts
@@ -0,0 +1,34 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '../../app/store';
+
+
+enum NavOptions {
+  home,
+  work,
+  posts,
+  contact
+}
+
+export interface NavState {
+  page: NavOptions
+}
+
+const initialState: NavState = {
+  page: NavOptions.home
+}
+
+export const navSlice = createSlice({
+  name: 'nav',
+  initialState,
+  reducers: {
+    switchPage: (state, action: PayloadAction<NavOptions>) => {
+      state.page = action.payload
+    }
+  }
+})
+
+export const { switchPage } = navSlice.actions
+
+export const selectNavPage = (state: RootState) => state.nav.page
+
+export default navSlice.reducer
