ad93b4d35d0d7dc3b33c7eac37e62697914af810
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-05 20:31:03 -0500

    site ported so far, so moved all code pertaining to old version of site to trash

--- old/core/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from .views import *
-
-
-urlpatterns = [
-    path(r'', index, name='index'),
-    path(r'posts/three-js-blob', threeJSBlob, name="three-js-blob")
-]
--- old/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-
-def index(request):
-    return render(request, 'home.html')
-
-def threeJSBlob(request):
-    return render(request, 'three-js-blob.html')
