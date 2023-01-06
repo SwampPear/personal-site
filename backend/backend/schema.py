9b47fc8d054e796a873e9c26946a026e5d9e0e08
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-06 12:45:34 -0500

    confirmed that graphql works

--- backend/backend/schema.py
+++ backend/backend/schema.py
@@ -8,7 +8,8 @@ class PostType(DjangoObjectType):
     class Meta:
         model = Post
         fields = ('postId', 'title', 'reading_time', 'content')
-        
+
+
 class Query(graphene.ObjectType):
     all_posts = graphene.List(PostType)
     post_by_id = graphene.Field(PostType, name=graphene.Int(required=True))
@@ -22,4 +23,76 @@ class Query(graphene.ObjectType):
         except Post.DoesNotExist:
             return None
 
-schema = graphene.Schema(query=Query)
+
+class PostInput(graphene.InputObjectType):
+    postId = graphene.ID()
+    title = graphene.String()
+    reading_time = graphene.Int()
+    content = graphene.JSONString()
+
+
+class CreatePost(graphene.Mutation):
+    class Arguments:
+        post_data = PostInput(required=True)
+
+    post = graphene.Field(PostType)
+
+    @staticmethod
+    def mutate(root, info, post_data=None):
+        post_instance = Post( 
+            postId=post_data.postId,
+            title=post_data.title,
+            reading_time=post_data.reading_time,
+            content=post_data.content
+        )
+
+        post_instance.save()
+
+        return CreatePost(book=post_instance)
+
+
+class UpdatePost(graphene.Mutation):
+    class Arguments:
+        post_data = PostInput(required=True)
+
+    post = graphene.Field(PostType)
+
+    @staticmethod
+    def mutate(root, info, post_data=None):
+
+        post_instance = Post.objects.get(pk=post_data.postId)
+
+        if post_instance:
+            post_instance.postId = post_data.postId
+            post_instance.title = post_data.title
+            post_instance.reading_time = post_data.reading_time
+            post_instance.content = post_data.content
+        
+            post_instance.save()
+
+            return UpdatePost(post=post_instance)
+
+        return UpdatePost(post=None)
+
+
+class DeletePost(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+
+    post = graphene.Field(PostType)
+
+    @staticmethod
+    def mutate(root, info, id):
+        post_instance = Post.objects.get(pk=id)
+        post_instance.delete()
+
+        return None
+
+
+class Mutation(graphene.ObjectType):
+    create_post = CreatePost.Field()
+    update_post = UpdatePost.Field()
+    delete_post = DeletePost.Field()
+
+
+schema = graphene.Schema(query=Query, mutation=Mutation)
