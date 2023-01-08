3e5a4839b359aa12098a1e71efcbd14f7d073df9
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-07 20:13:47 -0500

    implemented page to navigate through works

---
+++ frontend/src/features/works/Works.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './Works.module.css'
+import img from '../../image.jpeg'
+import Post from '../post/Post';
+
+import { useAppSelector, useAppDispatch } from '../../app/hooks'
+import { selectDarkModeState, selectDarkModeInit } from '../darkmode/darkModeSlice'
+
+import { useQuery, gql } from "@apollo/client";
+
+const QUERY = gql`
+  query {
+    allWorks {
+      workId
+      title
+      content
+    }
+  }
+`
+
+
+const Posts = () => {
+  const dispatch = useAppDispatch()
+  const darkmode = useAppSelector(selectDarkModeState)
+  const darkmodeInit = useAppSelector(selectDarkModeInit)
+
+  const { data, loading, error } = useQuery( QUERY, { pollInterval: 500 } )
+
+  if (error) return <p style={{marginTop: '35rem'}}>Error</p>;
+  if (loading) return <p style={{marginTop: '5rem'}}>Loading...</p>;
+
+  return (
+    <div className={styles.container}>
+      {
+        data.allWorks.map((work: any) => {
+          return (
+            <Post img={img} title={work.title}/>
+          )
+        })
+      }
+    </div>
+  )
+}
+
+export default Posts
