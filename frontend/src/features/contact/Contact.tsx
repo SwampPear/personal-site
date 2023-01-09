2a962c0e5ed7566583bf748f6bb0b47c553ac489
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-08 19:05:13 -0500

    implemented frontencd for contact form

--- frontend/src/features/contact/Contact.tsx
+++ frontend/src/features/contact/Contact.tsx
@@ -6,28 +6,28 @@ const Contact = () => {
 
   return (
     <div className={`${styles.container} ${styles.fadeIn}`}>
-      <div className="login">                                                                                                                                                                                                                       
-      <h2>Login</h2>                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-      <form method="post">                                                                                                                                                                                                                                                                                                                                                                                                                               
-        <div className="login__input">                                                                                                                                                                                                            
-          <input type="text" name="username"                                                                                                                                                                                                  
-          autoFocus={true} autoCapitalize="none" autoComplete="username"                                                                                                                                                                          
-          maxLength={32} required={true} id="id_username"/>                                                                                                                                                                                        
-          <label>MyID</label>                                                                                                                                                                                                                 
-        </div>                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                              
-        <div className="login__input">                                                                                                                                                                                                            
-          <input type="password" name="password"                                                                                                                                                                                              
-          autoComplete="current-password" required={true} id="id_password"/>                                                                                                                                                                       
-          <label>Password</label>                                                                                                                                                                                                             
-        </div>                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                              
-        <div className="login__button-wrapper">                                                                                                                                                                                                   
-          <button className="button button-blue" type="submit">Login</button>                                                                                                                                                                     
-        </div>                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                              
+      <form method="post">
+        <div className={styles.row}>
+          <div className={styles.input}>
+            <input type="text" name="username"                                                                                                                                                                                                  
+            autoFocus={true} autoCapitalize="none" autoComplete="username"                                                                                                                                                                          
+            maxLength={32} required={true}/>
+            <label>Name</label>
+          </div>
+          <div className={styles.input}>
+            <input type="text" name="email"                                                                                                                                                                                                  
+            autoFocus={true} autoCapitalize="none" autoComplete="email"                                                                                                                                                                          
+            maxLength={64} required={true}/>
+            <label>Email</label>
+          </div>
+        </div>
+        <div className={styles.textarea}>
+          <label>What can I help you with?</label>
+          <textarea name="inquiry"                                                                                                                                                                                                  
+          autoFocus={true} autoCapitalize="none" autoComplete="inquiry"                                                                                                                                                                          
+          maxLength={32} required={true}/>
+        </div>
       </form>                                                                                                                                                                                                                                 
-    </div>      
     </div> 
   )
 }
