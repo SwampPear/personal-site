e159a40a4ef3833fe118b97a256bc980fb6eaeac
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-09 14:58:47 -0500

    updated 'works' page to include fade in animation

--- frontend/src/features/works/Works.tsx
+++ frontend/src/features/works/Works.tsx
@@ -30,7 +30,7 @@ const Posts = () => {
   if (loading) return <p style={{marginTop: '5rem'}}>Loading...</p>;
 
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} ${styles.fadeIn}`}>
       {
         data.allWorks.map((work: any) => {
           return (
