5f04dfa22d7be3759f8d44a8d3e33df88f519d84
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-11 21:26:41 -0500

    will recreate project in normal django site for seo purposes

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "513f87ede56fb845"; }
+/******/ 		__webpack_require__.h = function() { return "58f8d1b187d2cf54"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
