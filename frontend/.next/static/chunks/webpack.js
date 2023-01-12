c488bb8c0563830db283b0a8d9dcd9ec76f48c9e
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-12 13:44:34 -0500

    confirmed that contacts can be made

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "580bc6bfc16839d6"; }
+/******/ 		__webpack_require__.h = function() { return "995bacca9d9a1a62"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/src/features/contact/Contact.tsx
+++ frontend/src/features/contact/Contact.tsx
@@ -1,26 +1,81 @@
 import styles from './Contact.module.css'
+import React, { useState } from 'react'
 
 import { selectDarkModeState, selectDarkModeInit } from '../darkmode/darkModeSlice'
 import { useAppSelector } from '../../app/hooks'
+
+import { gql, useMutation } from '@apollo/client'
+import { client } from '../../../pages/_app'
+
+
+const createContact = (name: string, email: string, content: string) => {
+  return gql`
+  mutation {
+    createContact(contactData: {name: "${name}", email: "${email}", content:"${content}"}) {
+      contact {
+        name
+        email
+        content
+      }
+    }
+  }`
+}
 
 
 const Contact = () => {
   const darkmode = useAppSelector( selectDarkModeState )
   const darkmodeInit = useAppSelector( selectDarkModeInit )
 
+  const [ name, setName ] = useState( '' )
+  const [ email, setEmail ] = useState( '' )
+  const [ content, setContent ] = useState( '' )
+
+  const [ sendDat ] = useMutation( createContact( name, email, content ) )
+
+  const sendData = () => {
+    client.mutate({
+      mutation: createContact( name, email, content )
+    })
+  }
+
+  const updateNameValue = ( e: React.FormEvent<HTMLInputElement> ) => {
+    const value = e.currentTarget.value
+ 
+    setName( value )
+  }
+
+  const updateEmailValue = ( e: React.FormEvent<HTMLInputElement> ) => {
+    const value = e.currentTarget.value
+ 
+    setEmail( value )
+  }
+
+  const updateContentValue = ( e: React.FormEvent<HTMLTextAreaElement> ) => {
+    const value = e.currentTarget.value
+ 
+    setContent( value )
+  }
+
+
   return (
     <div className={styles.container}>
-      <form method="post">
+      <form onSubmit={ ( e: React.SyntheticEvent ) => {
+          e.preventDefault()
+          sendData()
+        } } 
+        method='post'
+      >
         <div className={styles.row}>
           <div className={styles.input}>
             <input 
-              type="text" 
-              name="username"                                                                                                                                                                                                  
+              type='text'
+              name='username'                                                                                                                                                                                                  
               autoFocus={true} 
-              autoCapitalize="none" 
-              autoComplete="username"                                                                                                                                                                          
+              autoCapitalize='none'
+              autoComplete='username'                                                                                                                                                                          
               maxLength={127} 
               required={true}
+              onChange={updateNameValue}
               className={`${darkmodeInit ? (darkmode ? styles.inputToDark : styles.inputFromDark) : ''}`}
             />
             <label className={`${darkmodeInit ? (darkmode ? styles.textToDark : styles.textFromDark) : ''}`}>
@@ -36,6 +91,7 @@ const Contact = () => {
               autoComplete="email"                                                                                                                                                                          
               maxLength={255} 
               required={true}
+              onChange={updateEmailValue}
               className={`${darkmodeInit ? (darkmode ? styles.inputToDark : styles.inputFromDark) : ''}`}
             />
             <label className={`${darkmodeInit ? (darkmode ? styles.textToDark : styles.textFromDark) : ''}`}>
@@ -54,11 +110,12 @@ const Contact = () => {
             autoComplete="inquiry"                                                                                                                                                                          
             maxLength={2047} 
             required={true}
+            onChange={updateContentValue}
             className={`${darkmodeInit ? (darkmode ? styles.inputToDark : styles.inputFromDark) : ''}`}
           />
         </div>
         <div className={styles.submitButtonContainer}>
-          <button className={styles.submitButton}>Submit</button>
+          <button type='submit' className={styles.submitButton}>Submit</button>
         </div>
       </form>                                                                                                                                                                                                                                 
     </div>
