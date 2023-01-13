843ad184885d8c7bc031cb201a58030517ab2453
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-12 19:35:46 -0500

    changed name of backend admin portal

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-
 from .models import Post, Contact
+
+
+admin.site.site_header = 'Michael Vaden Website Admin'
+admin.site.site_title = 'Michael Vaden Website Admin'
+admin.site.index_title = 'Michael Vaden Website Admin'
 
 admin.site.register(Post)
 admin.site.register(Contact)
--- frontend/.next/static/webpack/webpack.a121848b98fbbe6e.hot-update.js
+++ frontend/.next/static/webpack/webpack.a121848b98fbbe6e.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "3874b3bc505a9571"; }
+/******/ 	__webpack_require__.h = function() { return "470161047dda7e6a"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
