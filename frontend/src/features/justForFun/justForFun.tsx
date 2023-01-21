799abe7a12594e9fb44cb9bec17caeecded70c68
Michael Vaden <michaeljosephvaden03@gmail.com>
2023-01-21 15:43:13 -0500

    made just for fun page fixed position

--- frontend/src/features/justForFun/justForFun.tsx
+++ frontend/src/features/justForFun/justForFun.tsx
@@ -10,16 +10,18 @@ const JustForFun = () => {
     // setup
     var scene = new THREE.Scene()
 
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 )
+    var cameraO = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 )
+    let width = window.innerWidth
+    let height = window.innerHeight
+    let camera = new THREE.OrthographicCamera( -width / 2, width / 2, height / 2, -height / 2, 0.1, 1000)
 
-    camera.position.z = 10
+    camera.position.z = 5
 
     var renderer = new THREE.WebGLRenderer( { alpha: true, antialias: true } )
     renderer.setSize( window.innerWidth, window.innerHeight )
     renderer.setClearColor( 0x000000, 0 )
     renderer.setPixelRatio( window.devicePixelRatio )
     
-    // append to container div
     mountRef.current!.appendChild( renderer.domElement )
 
     // testing
@@ -30,7 +32,7 @@ const JustForFun = () => {
     var treematerial = new THREE.SpriteMaterial( { map: treetexture } ); 
     var treesprite = new THREE.Sprite( treematerial );
 
-    treesprite.scale.set( 1, 2, 1);
+    treesprite.scale.set( 100, 100, 100);
     treesprite.position.set(0, 1, 0);
     scene.add(treesprite);
   
